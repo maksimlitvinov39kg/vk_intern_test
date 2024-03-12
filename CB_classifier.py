0aac02b60d3bbe3504614255b7678e00686cfc7b
Maksim Litvinov <95618433+maksimlitvinov39kg@users.noreply.github.com>
2024-03-12 23:58:32 +0300

    Update CB_classifier.py

--- CB_classifier.py
+++ CB_classifier.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn.metrics import ndcg_score
 from catboost import Pool, CatBoostClassifier
 
-train_df = pd.read_csv('vk_train_df.csv')
-test_df = pd.read_csv('vk_test_df.csv')
+train_df = pd.read_csv('data/vk_train_df.csv')
+test_df = pd.read_csv('data/vk_test_df.csv')
 
 X_train = train_df.drop(['search_id', 'target'], axis=1)
 y_train = train_df['target']
